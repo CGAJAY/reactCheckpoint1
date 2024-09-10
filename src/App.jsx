16b052a2006b52bdd9e5c953bf8534fdd9ef6deb
CGAJAY <jmugendi737@gmail.com>
2024-09-10 19:42:28 +0300

    change the product.js to Product.json

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,9 @@ import Description from "./components/Description";
 import Image from "./components/Image";
 import Name from "./components/Name";
 import Price from "./components/Price";
-import product from "./Product";
+import product from "./Product.json";
 
 const App = () => {
-	const product = {
-		name: "Gaming Laptop",
-		price: "$1,499.99",
-		description:
-			"High-performance gaming laptop with a powerful GPU and fast refresh rate display.",
-		image:
-			"https://images.unsplash.com/photo-1640695257754-7e2932f9ad0f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	};
-
 	console.log(product);
 
 	return (
